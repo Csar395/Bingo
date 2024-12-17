b834bdf09454cb8ec97fc2939cef6890a72fb016
isaak <isaakwilms@gmail.com>
2024-12-17 19:31:26 +0100

    Add responsive hamburger menu with off-screen navigation
    This update introduces a responsive hamburger menu for improved navigation, especially on smaller screens. The menu includes buttons for Group, Settings, Profile, and Logout, with smooth transitions and adaptive styles. Media queries ensure proper display across various screen sizes.

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
     updateUI();
 });
 
+const hamMenu = document.querySelector(".hamburger-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+});
+
 const loginButton = document.getElementById('login-button');
 loginButton.addEventListener('click', () => {
     localStorage.removeItem('actualState');
